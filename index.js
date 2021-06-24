34a92eb06a072258757001c1b9654e6d8b76a2c7
Sebastian Kraft <sebastian.kraft@buschglatz.com>
2021-06-24 04:08:07 +0200

    fix assertions

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ const endOfWeek23 = '13.06.2021';
 const year = '21';
 const week = 23;
 
-assert.strictEqual(startOfWeek23, dayjs(startOfWeek23, 'DD.MM.YYYY').format('DD.MM.YYYY'));
-assert.strictEqual(endOfWeek23, dayjs(endOfWeek23, 'DD.MM.YYYY').format('DD.MM.YYYY'));
-assert.strictEqual('2021', dayjs(year, 'YY').format('YYYY'));
-assert.strictEqual(startOfWeek23, dayjs(year, 'YY').isoWeek(week).startOf('isoWeek').format('DD.MM.YYYY'));
-assert.strictEqual(endOfWeek23, dayjs(year, 'YY').isoWeek(week).endOf('isoWeek').format('DD.MM.YYYY'));
+assert.strictEqual(dayjs(startOfWeek23, 'DD.MM.YYYY').format('DD.MM.YYYY'), startOfWeek23);
+assert.strictEqual(dayjs(endOfWeek23, 'DD.MM.YYYY').format('DD.MM.YYYY'), endOfWeek23);
+assert.strictEqual(dayjs(year, 'YY').format('YYYY'), '2021');
+assert.strictEqual(dayjs(year, 'YY').isoWeek(week).startOf('isoWeek').format('DD.MM.YYYY'), startOfWeek23);
+assert.strictEqual(dayjs(year, 'YY').isoWeek(week).endOf('isoWeek').format('DD.MM.YYYY'), endOfWeek23);

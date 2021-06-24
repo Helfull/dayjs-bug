b47b50d04e76ba0e7bff9183aa1646ad9bf1ea0d
Sebastian Kraft <sebastian.kraft@buschglatz.com>
2021-06-24 04:16:31 +0200

    show difference between parsing and year method

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ const isoWeek = require('dayjs/plugin/isoWeek');
 dayjs.extend(customParseFormat);
 dayjs.extend(isoWeek);
 
-assert.strictEqual(dayjs('21', 'YY').format('YYYY'), '2021');
+assert.strictEqual(dayjs().year(2021).format('YYYY'), '2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(22).format('YYYY'), '2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(23).format('YYYY'), '2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(24).format('YYYY'), '2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(22).startOf('isoWeek').format('DD.MM.YYYY'), '31.05.2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(22).endOf('isoWeek').format('DD.MM.YYYY'), '06.06.2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(23).startOf('isoWeek').format('DD.MM.YYYY'), '07.06.2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(23).endOf('isoWeek').format('DD.MM.YYYY'), '13.06.2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(24).startOf('isoWeek').format('DD.MM.YYYY'), '14.06.2021');
+assert.strictEqual(dayjs().year(2021).isoWeek(24).endOf('isoWeek').format('DD.MM.YYYY'), '20.06.2021');
+
 assert.strictEqual(dayjs('21', 'YY').format('YYYY'), '2021');
 assert.strictEqual(dayjs('21', 'YY').isoWeek(22).format('YYYY'), '2021');
 assert.strictEqual(dayjs('21', 'YY').isoWeek(23).format('YYYY'), '2021');
